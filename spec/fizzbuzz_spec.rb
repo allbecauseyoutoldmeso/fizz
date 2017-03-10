5cdb81bcc241c98f0a47de21337db040c2bc4beb
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-03-10 11:04:53 +0000

    returns fizz, buzz, fizzbuzz or original argument

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,6 +5,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
   it 'returns "buzz" when passed 10' do
-    expect(fizzbuzz(10)).to eq 'buzz' 
+    expect(fizzbuzz(10)).to eq 'buzz'
   end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'returns argument if argument not factor of 3 or 5' do
+    expect(fizzbuzz(8)).to eq 8
+  end  
+
 end
